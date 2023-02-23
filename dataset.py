a1f5e833463219b34d9abdd7178e591e2303e5c1
Amirpasha Mobini Tehrani <105310430+amirpashamobinitehrani@users.noreply.github.com>
2023-02-23 12:43:57 +0100

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -22,6 +22,37 @@ import torchaudio
 import pcen
 
 
+#Per-Channel Energy Normalization function
+def pcenfunc(x, eps=1E-6, 
+                s=0.025, 
+                alpha=0.98, 
+                delta=2, 
+                r=0.5, 
+                training=False):
+    
+    frames = x.split(1, -2)
+    m_frames = []
+    last_state = None
+    for frame in frames:
+        if last_state is None:
+            last_state = s * frame
+            m_frames.append(last_state)
+            continue
+        if training:
+            m_frame = ((1 - s) * last_state).add_(s * frame)
+        else:
+            m_frame = (1 - s) * last_state + s * frame
+        last_state = m_frame
+        m_frames.append(m_frame)
+    M = torch.cat(m_frames, 1)
+    if training:
+        pcen_ = (x / (M + eps).pow(alpha) + delta).pow(r) - delta ** r
+    else:
+        pcen_ = x.div_(M.add_(eps).pow_(alpha)).add_(delta).pow_(r).sub_(delta ** r)
+    return pcen_  
+
+
+
 class DataProcessing:
 
     '''
@@ -44,14 +75,18 @@ class DataProcessing:
     
     '''
     def __init__(self):
-        self.pcen = pcen.StreamingPCENTransform(n_mels=257, 
-                                                n_fft=512, 
-                                                hop_length=128, 
-                                                trainable=True)
         self.n_fft = 512
+        self.n_mels = self.n_fft // 2 + 1
         self.hop_length = 128
-        self.n_mels = 257
-
+        self.sample_rate = 16000
+        self.mel = torchaudio.transforms.MelSpectrogram(sample_rate=self.sample_rate, 
+                                                        n_mels = self.n_mels, 
+                                                        n_fft = self.n_fft, 
+                                                        hop_length = self.hop_length)
+        
+        self.atob = torchaudio.transforms.AmplitudeToDB(stype="amplitude", top_db=80)
+        #self.pcen = pcen  
+    
     
     def mod_phase(self, magnitude, real_demod, imag_demod):
         """
@@ -70,7 +105,7 @@ class DataProcessing:
 
         #construct complex spectrogram
         complex_spectrogram = torch.exp(magnitude) * torch.exp(1j * wrap)
-        return complex_spectrogram.unsqueeze(0)
+        return complex_spectrogram
 
 
 
@@ -98,13 +133,13 @@ class DataProcessing:
         real_demod = torch.sin(demodulated_phase)
         imag_demod = torch.cos(demodulated_phase)
 
-        return real_demod.requires_grad_(True), imag_demod.requires_grad_(True)
+        return real_demod, imag_demod
 
 
     def log_mag(self, spectrogram):
-        x = torch.log(torch.abs(spectrogram))
-        return torch.tensor(x, requires_grad=True)
-    
+        x = torch.abs(spectrogram)
+        x = torch.log(x)
+        return x
     
     
     def istft(self, spectrogram):
@@ -114,7 +149,6 @@ class DataProcessing:
         return torch.istft(spectrogram,
                              n_fft=self.n_fft,
                              hop_length=self.hop_length)
-
     
     def _stft(self, signal):
 
@@ -125,16 +159,15 @@ class DataProcessing:
                             n_fft=self.n_fft, 
                             hop_length=self.hop_length,
                             return_complex=True)
+            
 
-    def normalise(self, data):
-        return torch.nn.functional.normalize(data, 
-                                             p=2.0, 
-                                             dim=data.shape[-1], 
-                                             eps=1e-12)   
 
     def _pcen(self, signal):
-        x = self.pcen(signal)
-        return x
+        #construct spectrogram complex --> float32
+        mel = self.mel(signal)
+        amp_to_db = self.atob(mel)
+        to_pcen = pcenfunc(mel.permute((0,2,1)))
+        return to_pcen.permute((0, 2, 1))
 
     
     def perm(self, tensor):
@@ -143,9 +176,18 @@ class DataProcessing:
         '''
         return tensor.permute(2, 0, 1)
    
-   
     
+    def normalise(self, audio):
+        audio = audio.squeeze(0)
+        mean = torch.mean(audio)
+        std = torch.std(audio)
+        norm_audio = (audio - mean) / std
+        return norm_audio.unsqueeze(0)
+   
     def __call__(self, audio):
+
+        #normalise audio
+        audio = self.normalise(audio)
         
         #get spectrogram from audio
         spectrogram = self._stft(audio)
@@ -155,16 +197,14 @@ class DataProcessing:
         real_demod, imag_demod = self._demod_phase(spectrogram)
 
         #calculate PCEN
-        pcen = self._pcen(audio).permute(0, 2, 1)
+        pcen = self._pcen(audio)
         
-        #concatenate and permute data to be fed to the network
-        data = torch.cat((self.perm(log_magnitude.requires_grad_(False)),
-                          self.perm(pcen).detach(),
-                          self.perm(real_demod.requires_grad_(False)),
-                          self.perm(imag_demod.requires_grad_(False))), dim = 1)
-       
-        #normalaise data
-        #data = self.normalise(data)
+        #concatenate and permute data to be fed to the network 
+        data = torch.cat((self.perm(log_magnitude),
+                          self.perm(pcen),
+                          self.perm(real_demod),
+                          self.perm(imag_demod)), dim = 1)
+        
         
         #returns data of structure (time_frame, 4 features, freq_bins)
         return data
