53e3780063328e92cce6af465b2320c41f9c2b62
Amirpasha Mobini Tehrani <105310430+amirpashamobinitehrani@users.noreply.github.com>
2023-02-23 13:52:09 +0100

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -1,3 +1,31 @@
+import os
+import numpy as np
+import librosa
+import pcen
+
+from scipy.io.wavfile import read as wavread
+import warnings
+warnings.filterwarnings("ignore")
+
+import torch
+import torch.nn as nn
+import torchaudio
+from torch.utils.data import Dataset
+from torch.utils.data.distributed import DistributedSampler
+from torchaudio import transforms
+
+import random
+random.seed(0)
+torch.manual_seed(0)
+np.random.seed(0)
+
+from torchvision import datasets, models, transforms
+import torchaudio
+import pcen
+
+
+
+#Calcualate Per-Channel Energy Normalization
 def pcenfunc(x, eps=1E-6, s=0.025, alpha=0.98, delta=2, r=0.5, training=False):
     frames = x.split(1, -2)
     m_frames = []
@@ -19,6 +47,8 @@ def pcenfunc(x, eps=1E-6, s=0.025, alpha=0.98, delta=2, r=0.5, training=False):
     else:
         pcen_ = x.div_(M.add_(eps).pow_(alpha)).add_(delta).pow_(r).sub_(delta ** r)
     return pcen_  
+
+
 
 
 class DataProcessing:
@@ -151,9 +181,11 @@ class DataProcessing:
         norm_audio = (audio - mean) / std
         return norm_audio.unsqueeze(0)
    
-    
     def __call__(self, audio):
 
+        #normalise audio
+        #audio = self.normalise(audio)
+        
         #get spectrogram from audio
         spectrogram = self._stft(audio)
 
@@ -171,5 +203,138 @@ class DataProcessing:
                           self.perm(imag_demod)), dim = 1)
         
         
+        data = torch.nn.functional.normalize(data, dim=0)
         #returns data of structure (time_frame, 4 features, freq_bins)
-        return torch.nn.functional.normalize(data, dim=2)
+        return data
+
+
+
+class CleanNoisyPairDataset(Dataset):
+    """
+    Create a Dataset of clean and noisy audio pairs. 
+    Each element is a tuple of the form (clean_spectrogam, noisy_spectrogram, clean waveform, noisy waveform, file_id)
+    
+    Returns:
+       (Clean Spectrogram, Noisy Spectrogram, Clean Audio, Noisy Audio, Filed id)
+    """
+    
+    def __init__(self, root='./', subset='training', crop_length_sec=0):
+        super(CleanNoisyPairDataset).__init__()
+        
+
+        self.dp = DataProcessing()
+        
+        assert subset is None or subset in ["training", "testing"]
+        self.crop_length_sec = crop_length_sec
+        self.subset = subset
+
+        N_clean = len(os.listdir(os.path.join(root, 'training_set/clean')))
+        N_noisy = len(os.listdir(os.path.join(root, 'training_set/noisy')))
+        assert N_clean == N_noisy
+
+        if subset == "training":
+            self.files = [(os.path.join(root, 'training_set/clean', 'fileid_{}.wav'.format(i)),
+                           os.path.join(root, 'training_set/noisy', 'fileid_{}.wav'.format(i))) for i in range(N_clean)]
+        
+        elif subset == "testing":
+            sortkey = lambda name: '_'.join(name.split('_')[-2:])  # specific for dns due to test sample names
+            _p = os.path.join(root, 'datasets/test_set/synthetic/no_reverb')  # path for DNS
+            
+            clean_files = os.listdir(os.path.join(_p, 'clean'))
+            noisy_files = os.listdir(os.path.join(_p, 'noisy'))
+            
+            clean_files.sort(key=sortkey)
+            noisy_files.sort(key=sortkey)
+
+            self.files = []
+            for _c, _n in zip(clean_files, noisy_files):
+                assert sortkey(_c) == sortkey(_n)
+                self.files.append((os.path.join(_p, 'clean', _c), 
+                                   os.path.join(_p, 'noisy', _n)))
+            self.crop_length_sec = 0
+
+        else:
+            raise NotImplementedError 
+
+    
+
+    def __getitem(self, n):
+        fileid = self.files[n]
+        clean_audio, sample_rate = librosa.load(fileid[0])
+        noisy_audio, sample_rate = librosa.load(fileid[1])
+        clean_audio, noisy_audio = clean_audio, noisy_audio
+        assert len(clean_audio) == len(noisy_audio)
+
+        crop_length = int(self.crop_length_sec * sample_rate)
+        assert crop_length < len(clean_audio)
+            
+        #random crop in the time domain
+        if self.subset != 'testing' and crop_length > 0:
+            start = np.random.randint(low=0, high=len(clean_audio) - crop_length + 1)
+            clean_audio = clean_audio[start:(start + crop_length)]
+            noisy_audio = noisy_audio[start:(start + crop_length)]
+
+        #prepare audio signal and spectrogram data pairs
+        clean_audio, noisy_audio = clean_audio, noisy_audio
+        clean_features, noisy_features = self.dp(clean_audio), self.dp(noisy_audio)
+        
+        #make input shape suitable for network
+        return (clean_features, noisy_features, 
+                clean_audio, noisy_audio, 
+                fileid)
+
+    
+    def __len__(self):
+        return len(self.files)
+
+
+def load_CleanNoisyPairDataset(root,
+                               subset,
+                               crop_length_sec,
+                               batch_size,
+                               sample_rate,
+                               num_gpus = 1):
+        
+        dataset = CleanNoisyPairDataset(root = root, subset = subset, crop_length_sec=crop_length_sec)
+        kwargs = {'batch_size': batch_size,
+                  'num_workers': 4,
+                  'pin_memory': False,
+                  'drop_last': False}
+        
+        if num_gpus > 1:
+            train_sampler = DistributedSampler(dataset)
+            dataloader = torch.utils.data.Dataloader(dataset, sampler=train_sampler, **kwargs)
+        else:
+            dataloader = torch.utils.data.Dataloader(dataset, sampler=None, shuffle=True, **kwargs)
+        
+        return dataloader
+
+
+if __name__ == '__main__':
+    import json
+    with open('./configs/tiny.json') as f:
+        data = f.read()
+    config = json.loads(data)
+    trainset_config = config('train')
+
+    trainloader = load_CleanNoisyPairDataset(**trainset_config,
+                                             subset='training',
+                                             batch_size=2,
+                                             num_gpus=1)
+    testloader = load_CleanNoisyPairDataset(**trainset_config,
+                                            subset='testing',
+                                            batch_size=2,
+                                            num_gpus=1)
+    print(len(trainloader), len(testloader))
+
+    for clean_feat, noisy_feat, clean_audio, noisy_audio, fileid in trainloader:
+        
+        clean_feat = clean_feat.cuda()
+        noisy_feat = noisy_feat.cuda()
+        
+        clean_audio = clean_audio.cuda()
+        noisy_audio = noisy_audio.cuda()
+        
+        print(clean_feat.shape, noisy_audio.shape, fileid)
+        print(clean_audio.shape, noisy_audio.shape, fileid)
+        break       
