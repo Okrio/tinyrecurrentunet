06a7f536484bac07424790b59ecfeaaf791013ab
Amirpasha Mobini Tehrani <105310430+amirpashamobinitehrani@users.noreply.github.com>
2023-03-02 14:37:09 +0100

    Update train.py

--- train.py
+++ train.py
@@ -100,6 +100,7 @@ def train(num_gpus,
     n_iter = ckpt_iter + 1
 
     # define learning rate scheduler and stft-loss
+    """
     scheduler = LinearWarmupCosineDecay(
                     optimizer,
                     lr_max=optimization["learning_rate"],
@@ -109,6 +110,8 @@ def train(num_gpus,
                     warmup_proportion=0.05,
                     phase=('linear', 'cosine'),
                 )
+    """
+          
 
     if loss_config["stft_lambda"] > 0:
         mrstftloss = MultiResolutionSTFTLoss(**loss_config["stft_config"]).cuda()
@@ -137,7 +140,7 @@ def train(num_gpus,
             # back-propagation
             loss.backward()
             grad_norm = nn.utils.clip_grad_norm_(net.parameters(), 1e9)
-            scheduler.step()
+            #scheduler.step()
             optimizer.step()
 
             # output to log
