72d47de8eeded20aa1300f25a9ede8de8e40164e
Amirpasha Mobini Tehrani <105310430+amirpashamobinitehrani@users.noreply.github.com>
2023-03-08 11:55:00 +0100

    Update rt.py

--- rt.py
+++ rt.py
@@ -3,6 +3,7 @@ import torch
 import json
 import argparse
 import os
+import onnxruntime as rt
 
 from network import TRUNet
 
@@ -14,8 +15,31 @@ def load_model(model_path, network_config):
     model.load_state_dict(ckpt['model_state_dict'])
     model.eval()
     return model
-  
-  
+
+#torch inference function
+def torch_inference(torch_model):
+    x = torch.randn((1, 4, 257), dtype = torch.float32, requires_grad = False)
+    start_time = time.time()
+    with torch.no_grad():
+      y = model(x)
+ 
+    end_time = time.time()
+    return =  end_time - start_time 
+
+
+#onnx inference function
+def onnx_inference(onnx_model_path):
+    x = torch.randn(1, 4 , 257, dtype = torch.float32)
+    ort_session = rt.InferenceSession(onnx_model_path)
+    ort_input = {ort_session.get_inputs()[0].name: to_numpy(x)}
+    
+    #inference
+    start_time = time.time()
+    ort_outs = ort_session.run(None, ort_input)
+    end_time = time.time()
+    
+    return (end_time - start_time)
+    
   
 def time_average(lst):
   return sum(lst) / len(lst)
@@ -26,6 +50,7 @@ if __name__ == "__main__":
   parser = argparse.ArgumentParser()
   parser.add_argument('-c', '--config', type=str, help = 'Path to config Json file')
   parser.add_argument('-i', '--ckpt_path', type=str, help = 'Path to trained torch model')
+  parser.add_argument('-x', '--onnx_path', type=str, help = 'Path to onnx model')
   args = parser.parse_args()
 
   #load json file
@@ -33,24 +58,27 @@ if __name__ == "__main__":
     data = f.read()
   config = json.loads(data)
   
-  #load model
+  #load models
   model_config = config["network"]
-  model = load_model(args.ckpt_path,
+  torch_model = load_model(args.ckpt_path,
                     model_config)
+  onnx_model = args.onnx_path
   
-  #create dummy input
   
-  time_keeper = []
+  #list to save inference time in each in time
+  time_keeper_torch = []
+  time_keeper_onnx = []
   
-  for i in range(751):
-    x = torch.randn((1, 4, 257),  requires_grad = False)
-    start_time = time.time()
-    with torch.no_grad():
-      y = model(x)
- 
-    end_time = time.time()
-    inf_time = (end_time - start_time)
-    time_keeper.append(inf_time)
+  for i in range(751): 
+    torch_time = torch_inference(torch_model)
+    onnx_time = onnx_inference(onnx_model)
+    
+    time_keeper.append(torch_time)
+    time_keeper.append(onnx_time)
    
-  avg_inf_time = time_average(time_keeper)
-  print(f"Average inference time in ms: {avg_inf_time}")
+  avg_torch_time = time_average(time_keeper_torch)
+  avg_onnx_time = time_average(time_keeper_onnx)
+  
+  print("Average inference times in ms:")
+  print(f"Torch: {avg_inf_time}")
+  print(f"ONNX:  {avg_inf_time}")
