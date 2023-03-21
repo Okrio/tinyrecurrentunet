cc39ab98e21de17e85d449813905b4d9eb223fe8
Amirpasha Mobini Tehrani <105310430+amirpashamobinitehrani@users.noreply.github.com>
2023-03-21 12:00:39 +0100

    Update util.py

--- util.py
+++ util.py
@@ -244,14 +244,13 @@ def loss_fn(net, X, ell_p, ell_p_lambda, stft_lambda, mrstftloss, **kwargs):
     loss = 0.0
     
     #from time-domain to 4-features (log-Mag Spec, PCEN, demod Real, demod Imag)
-    #clean_feat = dp(clean_audio)
-    #noisy_feat = dp(noisy_audio)
+    noisy_feat = dp(noisy_audio)
     
     #forward propagation
-    denoised_audio = net(noisy_audio)  
+    denoised_feat = net(noisy_feat)  
     
     #convert features back to time-domain
-    denoised_mag, denoised_pcen, denoised_real, denoised_imag = denoised_audio.permute(1, 0, 2)
+    denoised_mag, denoised_pcen, denoised_real, denoised_imag = denoised_feat.permute(1, 0, 2)
     _, clean_pcen, _, _ = clean_audio.permute(1, 0, 2)
     
     #reverse function of demodulate - to convert back to audio
@@ -260,13 +259,13 @@ def loss_fn(net, X, ell_p, ell_p_lambda, stft_lambda, mrstftloss, **kwargs):
                             denoised_imag)
     
     
-    modulate_clean = mod_phase(clean_audio.permute(1, 0, 2)[0],
-                               clean_audio.permute(1, 0, 2)[2],
-                               clean_audio.permute(1, 0, 2)[3])
+    #modulate_clean = mod_phase(clean_audio.permute(1, 0, 2)[0],
+    #                           clean_audio.permute(1, 0, 2)[2],
+    #                           clean_audio.permute(1, 0, 2)[3])
     
     #Spectrogram to Waveform
     denoised_audio = istft(modulate_denoised.permute(0, 2, 1))
-    clean_audio = istft(modulate_clean.permute(0, 2, 1))
+    
     
     # calculate Cosine Similarity Loss
     cs_loss = cs(denoised_audio, clean_audio)
