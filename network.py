dd48ac8db5ab0754aacc6bba93e2fbbb37a1bc6d
Amirpasha Mobini Tehrani <105310430+amirpashamobinitehrani@users.noreply.github.com>
2023-06-06 19:52:22 +0200

    Update network.py

--- network.py
+++ network.py
@@ -6,7 +6,6 @@ from torch.nn import *
 from phm import PhaseAwareMask
 
 #TRUNet with 1 dimensional convolutions
-
 class StandardConv1d(nn.Module):
     def __init__(self, in_channels, out_channels, kernel_size, stride):
         super(StandardConv1d, self).__init__()
@@ -129,40 +128,46 @@ class TRUNet(nn.Module):
                 kernel_sizes,
                 strides,
                 tr_channels_input):
+        
         super(TRUNet, self).__init__()
-        self.down1 = StandardConv1d(3,64,5,2)
-        self.down2 = DepthwiseSeparableConv1d(64, 128, 3, 1)
-        self.down3 = DepthwiseSeparableConv1d(128, 128, 5, 2)
-        self.down4 = DepthwiseSeparableConv1d(128, 128, 3, 1)
-        self.down5 = DepthwiseSeparableConv1d(128, 128, 5, 2)
-        self.down6 = DepthwiseSeparableConv1d(128, 128, 3, 2)
+        
+        self.encoder = nn.ModuleList(nn.Sequential(self.down1 = StandardConv1d(3, 64, 5, 2),
+                                     DepthwiseSeparableConv1d(64, 128, 3, 1),
+                                     DepthwiseSeparableConv1d(128, 128, 5, 2),
+                                     DepthwiseSeparableConv1d(128, 128, 3, 1),
+                                     DepthwiseSeparableConv1d(128, 128, 5, 2),
+                                     DepthwiseSeparableConv1d(128, 128, 3, 2)))
+        
+        self.decoder = nn.ModuleList(FirstTrCNN(64, 64, 3, 2),
+                                     TrCNN(192, 64, 5, 2),
+                                     TrCNN(192, 64, 3, 1),
+                                     TrCNN(192, 64, 5, 2),
+                                     TrCNN(192, 64, 3, 1),
+                                     LastTrCNN(128, 8, 5, 2))
+        
+
         self.FGRU = GRUBlock(128, 64, 64, bidirectional=True)
         self.TGRU = GRUBlock(64, 128, 64, bidirectional=False)
-        self.up1 = FirstTrCNN(64, 64, 3, 2)
-        self.up2 = TrCNN(192, 64, 5, 2)
-        self.up3 = TrCNN(192, 64, 3, 1)
-        self.up4 = TrCNN(192, 64, 5, 2)
-        self.up5 = TrCNN(192, 64, 3, 1)
-        #self.up6 = LastTrCNN(128, 3, 5, 2)
-        self.up6 = LastTrCNN(128, 8, 5, 2)
+        
 
     def forward(self, x):
-        x1 = self.down1(x)
-        x2 = self.down2(x1)
-        x3 = self.down3(x2)
-        x4 = self.down4(x3)
-        x5 = self.down5(x4)
-        x6 = self.down6(x5)
-        x7 = x6.transpose(1,2)
-        x8 = self.FGRU(x7)
-        x9 = x8.transpose(1,2)
-        x10 = self.TGRU(x9)
-        x11 = self.up1(x10)
-        x12 = self.up2(x11,x5)
-        x13 = self.up3(x12,x4)
-        x14 = self.up4(x13,x3)
-        x15 = self.up5(x14,x2)
-        x16 = self.up6(x15,x1)
-        return x16
+        
+        skip_connections = []
+        
+        for down_sample in self.encoder:
+            x = down_sample(x)
+            skip_connections.append(x)
+        skip_connections = skip_connections[::-1]
+        
+        x = x.transpose(1, 2)
+        x = self.FGRU(x)
+        x = x.transpose(1, 2)
+        
+        for i, upsampling_block in enumerate(self.decoder):
+            skip_i = skip_connections[i]
+            x += skip_i[:, :, :x.shape[-1]]
+            x = upsampling_block(x)
+
+        return x
 
     
